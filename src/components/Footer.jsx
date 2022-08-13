82dc381367c0e3f4c5f891e42ba548d3cc4af0f5
Franklin <franklinpinedat@gmail.com>
2022-08-13 12:10:47 -0500

    project completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,10 @@
 export default function Footer() {
-    return <h1>Footer Here</h1>
+    return (
+      <footer className="footer-section">
+        <span className="footer-section__icon twitter"><a></a></span>
+        <span className="footer-section__icon facebook"><a></a></span>
+        <span className="footer-section__icon instagram"><a></a></span>
+        <span className="footer-section__icon github"><a></a></span>
+      </footer>
+    )
 }
