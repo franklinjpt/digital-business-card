8a9aa6c515502fc01259ddc87db291c5e06f1eaf
Franklin <franklinpinedat@gmail.com>
2022-08-13 11:30:41 -0500

    about section completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,8 @@
 export default function About() {
-    return <h1>About Here</h1>
+    return (
+      <section className="about-section">
+        <h2 className="about-section__title">About</h2>
+        <p className="about-section__description">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+      </section>
+    )
 }
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,3 +1,8 @@
 export default function Interests() {
-  return <h1>Interests Here</h1>
+  return (
+    <section className="about-section interests">
+      <h2 className="about-section__title">Interests</h2>
+      <p className="about-section__description">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </section>
+  )
 }
